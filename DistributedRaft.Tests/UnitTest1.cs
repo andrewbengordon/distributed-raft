ee92e5023b6f9e441a7eaac1179fe82aa1c6ac40
andrewbengordon <andrewbengordon@gmail.com>
2024-03-12 11:26:55 -0600

    should i even be doing ssl

---
+++ DistributedRaft.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace DistributedRaft.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
